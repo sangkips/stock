4196069640cfa4a19aafa165dc2383b833ff7f61
sangkips <sangkipkoech>
2024-06-13 22:43:14 +0300

    feat: add swagger restframework

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,47 @@
-
+from django.conf import settings
+from django.views.generic.base import RedirectView
 from django.contrib import admin
-from django.urls import path, include
+from decouple import config
+from django.conf.urls.static import static
+from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import re_path as url, include
+
+
+public_apis = [
+    # urls for the auth app
+    url(r'^api/v1/account/', include("apps.account.urls")),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title=config("API_TITLE"),
+        default_version=config("API_VERSION"),
+        description="These are the main APIs for VIC-Invest",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
+    path('developer/docs', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('developer/doc', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc'),
+    path('favicon.ico', favicon_view),
+    # urls for the tokens app
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += public_apis
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.site.site_header = config('ADMIN_SITE_HEADER')
+admin.site.site_title = config('ADMIN_SITE_TITLE')
+admin.site.index_title = config('ADMIN_SITE_INDEX_TITLE')
