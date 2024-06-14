7a5d31b5a8a3ef1775a4c0f4cbf9d7ea5d03bf3e
sangkips <sangkipkoech>
2024-06-14 21:34:22 +0300

    feat: add urls for new custom view

--- backend/apps/account/urls.py
+++ backend/apps/account/urls.py
@@ -1,3 +1,26 @@
+from django.urls import path
+from apps.account import views
 
-
-urlpatterns = []
+urlpatterns = [
+    # re_path(
+    #     r'^o/(?P<provider>\S+)/$',
+    #     CustomProviderAuthView.as_view(),
+    #     name='provider-auth'
+    # ),
+    path('jwt/create/',
+        views.CustomTokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path('jwt/refresh/',
+        views.CustomTokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path('jwt/verify/',
+        views.CustomTokenVerifyView.as_view(),
+        name='token_verify'
+    ),
+    path('logout/',
+        views.LogoutView.as_view(),
+        name='logout'
+    ),
+]
