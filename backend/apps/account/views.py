7486a0d77f904eff964df026dd2e936bad667770
sangkips <sangkipkoech>
2024-06-14 19:58:33 +0300

    feat: add views to handle https and cookies

--- backend/apps/account/views.py
+++ backend/apps/account/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
-# Create your views here.
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        
+        if response.status_code == 200:
+            access_token = response.data.get('access')
+            refresh_token = response.data.get('refresh')
+            
+            response.set_cookie(
+                'access',
+                access_token,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE,
+                secure=settings.AUTH_COOKIE_SECURE
+            )
+            
+            response.set_cookie(
+                'refresh',
+                refresh_token,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE,
+                secure=settings.AUTH_COOKIE_SECURE
+            )
+            
+        return response
+    
+class CustomTokenRefreshView(TokenRefreshView):
+    pass
