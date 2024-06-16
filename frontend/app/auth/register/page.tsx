59977b78b962ef751407837a32b55cc7f54a7163
sangkips <sangkipkoech>
2024-06-16 18:09:22 +0300

    feat: refactor register to use forms

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import Link from "next/link";
-import Spinner from "@/components/common/spinner";
+import { RegisterForm } from '@/components/forms';
+import Spinner from "@/components/common/Spinner";
 import { useRegister } from "@/hooks";
 
 export default function Page() {
@@ -19,6 +20,7 @@ export default function Page() {
     return (
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <RegisterForm />
          
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Register for an account
@@ -26,104 +28,6 @@ export default function Page() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" onSubmit={onSubmit}>
-            <div>
-              <label htmlFor="first_name" className="block text-sm font-medium leading-6 text-gray-900">
-                First Name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="first_name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  name="first_name"
-                  type="text"
-                  onChange={onChange}
-                  value={first_name}
-                  required
-                />
-              </div>
-            </div>
-
-            <div>
-              <label htmlFor="last_name" className="block text-sm font-medium leading-6 text-gray-900">
-                Last Name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="last_name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  name="last_name"
-                  type="text"
-                  onChange={onChange}
-                  value={last_name}
-                  required
-                />
-              </div>
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  onChange={onChange}
-                  value={email}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  onChange={onChange}
-                  value={password}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="re_password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Confirm Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="re_password"
-                  name="re_password"
-                  type="password"
-                  onChange={onChange}
-                  value={re_password}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                {isLoading ? <Spinner sm /> : 'Signup'}
-              </button>
-            </div>
-          </form>
-
           <p className="mt-10 text-center text-sm text-gray-500">
             Have an account?{' '}
             <Link
---
+++ frontend/components/forms/Input.tsx
@@ -0,0 +1,54 @@
+import { ChangeEvent } from 'react';
+import Link from 'next/link';
+
+
+interface Props{
+    type: string;
+    labelId: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    value: string;
+    children: React.ReactNode;
+    required?: boolean;
+    link?: {
+		linkText: string;
+		linkUrl: string;
+	};
+}
+export default function Input({
+    labelId,
+    type,
+    value,
+    onChange,
+    children,
+    required = false,
+    link,
+}: Props) {
+    return (
+        <div>
+            <label htmlFor="labelId" className="block text-sm font-medium leading-6 text-gray-900">
+                {children}
+            </label>
+            {link && (
+					<div className='text-sm'>
+						<Link
+							className='font-semibold text-indigo-600 hover:text-indigo-500'
+							href={link.linkUrl}
+						>
+							{link.linkText}
+						</Link>
+					</div>
+				)}
+            <div className="mt-2">
+                <input
+                    id={labelId}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    name={labelId}
+                    type={type}
+                    onChange={onChange}
+                    value={value}
+                    required={required}
+                />
+            </div>
+        </div>
+    )
+}
