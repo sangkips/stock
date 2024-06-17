ea6c79b6f582a0c7bd79f08bdb746dd63ba2a2b3
sangkips <sangkipkoech>
2024-06-17 11:08:13 +0300

    fix: update registration page functionality

--- frontend/app/activation/[uid]/[token]/page.tsx
+++ frontend/app/activation/[uid]/[token]/page.tsx
@@ -34,7 +34,7 @@ export default function Page({ params }: Props) {
     return (
         <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
 			<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-				<h1 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+				<h1 className='mt-10 text-center text-2xl leading-9 tracking-tight text-gray-900'>
 					Activating your account
 				</h1>
 			</div>
--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -1,29 +1,29 @@
-"use client";
-
 import Link from "next/link";
 import { RegisterForm } from '@/components/forms';
 
 export default function Page() {
 
-    return (
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <RegisterForm />
-         
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Register for an account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Have an account?{' '}
-            <Link
-              href="/auth/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              login
-            </Link>
-          </p>
-        </div>
+  return (
+    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+        <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+          Register new account
+        </h2>
       </div>
-    )
-  }
+
+      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+        <RegisterForm />
+
+        <p className='mt-10 text-center text-sm text-gray-500'>
+          Already have an account?{' '}
+          <Link
+            href='/auth/login'
+            className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+          >
+            Login here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
