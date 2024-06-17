0bd1a83edce79ea21a12db3583384f9e4ded2aa3
sangkips <sangkipkoech>
2024-06-17 11:12:31 +0300

    fix: update message on registration

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
         <LoginForm />
 
         <p className='mt-10 text-center text-sm text-gray-500'>
-          Already have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link
             href='/auth/register'
             className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
