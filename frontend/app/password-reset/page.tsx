35aba221a1eb06133458f0e03ad51b91defbbfed
sangkips <sangkipkoech>
2024-06-17 20:20:03 +0300

    feat: add reset password functionality page

--- frontend/app/password-reset/page.tsx
+++ frontend/app/password-reset/page.tsx
@@ -1,7 +1,19 @@
+import { ResetPasswordForm } from '@/components/forms';
+
+
 export default function Page() {
-    return (
-        <main>
-            <h1>Request reset pssword</h1>
-        </main>
-    )
+
+  return (
+    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+        <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+          Reset Password
+        </h2>
+      </div>
+
+      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+        <ResetPasswordForm />
+      </div>
+    </div>
+  );
 }
