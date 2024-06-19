cbcea3474f88cba1dfbd4b85b1b1b1f5286695fa
sangkips <sangkipkoech@gmail.com>
2024-06-19 17:35:48 +0300

    feat: add customer form

---
+++ frontend/components/forms/CreateCustomerForm.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useLogin, useRegister } from '@/hooks';
+import { Form } from '@/components/forms';
+import useCreateCustomer from '@/hooks/use-createcustomer';
+
+export default function CustomerForm() {
+    const {
+        first_name,
+        last_name,
+        middle_name,
+        phone,
+        address,
+        isLoading,
+        onChange,
+        onSubmit
+    } = useCreateCustomer();
+
+    const config = [
+    
+		{
+			labelText: 'First name',
+			labelId: 'first_name',
+			type: 'text',
+			value: first_name,
+			required: true,
+		},
+		{
+			labelText: 'Last name',
+			labelId: 'last_name',
+			type: 'text',
+			value: last_name,
+			required: true,
+        },
+        {
+			labelText: 'Middle name',
+			labelId: 'last_name',
+			type: 'text',
+			value: middle_name,
+			required: false,
+		},
+		{
+			labelText: 'Phone Number',
+			labelId: 'phone',
+			type: 'phone',
+            value: phone,
+			required: false,
+        },
+        {
+			labelText: 'Address',
+			labelId: 'address',
+			type: 'address',
+            value: address,
+			required: false,
+		},
+	];
+  
+  return (
+    <Form
+      config={config}
+      isLoading={isLoading}
+      btnText='Add customer'
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+}
