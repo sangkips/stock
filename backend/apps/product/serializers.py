b70ce4e43c0d4d7000486c99afa35e7f2b21390b
sangkips <sangkipkoech@gmail.com>
2024-06-20 23:12:26 +0300

    feat: add category serializer

--- backend/apps/product/serializers.py
+++ backend/apps/product/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from apps.product.models import Customer
+from apps.product.models import (
+    Category,
+    Customer,
+)
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
-        
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category 
+        fields = '__all__'
