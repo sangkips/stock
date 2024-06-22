412474d4c7de29f616f44f1eea4388dc2e929e72
sangkips <sangkipkoech@gmail.com>
2024-06-22 14:11:00 +0300

    feat: add create product form

---
+++ frontend/components/forms/CreateProductForm.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { Form } from '@/components/forms';
+import useCreateProduct from '@/hooks/use-createproduct';
+import useFetchCategories from '@/hooks/use-fetchcategories';
+
+export default function ProductForm() {
+    const {
+        name,
+        description,
+        quantity,
+        category,
+        isLoading,
+        onChange,
+        onSubmit
+	} = useCreateProduct();
+	
+	const { categories, isLoading: categoriesLoading, isError } = useFetchCategories();
+
+    const config = [
+    
+		{
+			labelText: 'Product Name',
+			labelId: 'name',
+			type: 'text',
+			value: name,
+			required: true,
+		},
+		{
+			labelText: 'Description',
+			labelId: 'description',
+			type: 'text',
+			value: description,
+			required: false,
+        },
+        {
+			labelText: 'Quantity',
+			labelId: 'quantity',
+			type: 'number',
+			value: quantity,
+			required: true,
+		},
+		{
+			labelText: 'Product Category',
+			labelId: 'category',
+			type: 'text',
+            value: category,
+			required: true,
+			options: categoriesLoading || isError ? [] : Array.isArray(categories) && categories?.map(cat => ({ value: cat.id, label: cat.name })),
+        },
+	];
+  
+  return (
+    <Form
+      config={config}
+      isLoading={isLoading}
+      btnText='Add Product'
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+}
