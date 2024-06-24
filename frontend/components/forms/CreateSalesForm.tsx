f2c3b97dd5f9e178b402167f77f1109b447b0d37
sangkips <sangkipkoech@gmail.com>
2024-06-24 18:43:18 +0300

    feat: create sales form

---
+++ frontend/components/forms/CreateSalesForm.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { Form } from '@/components/forms';
+import useCreateSales from '@/hooks/use-createsales';
+
+export default function SalesForm() {
+
+    const {
+        quantity,
+        price,
+        product,
+        customer,
+        customers,
+        products,
+        isLoading,
+        onChange,
+        onSubmit
+    } = useCreateSales();
+
+    const config = [
+        {
+            labelText: 'Product Quantity',
+            labelId: 'quantity',
+            type: 'number',
+            value: quantity,
+            required: true,
+        },
+        {
+            labelText: 'Price',
+            labelId: 'price',
+            type: 'number',
+            value: price,
+            required: true,
+        },
+        {
+            labelText: 'Product',
+            labelId: 'product',
+            type: 'select',
+            value: product,
+            required: true,
+            options: Array.isArray(products) ? products.map(prod => ({ value: prod.id.toString(), label: prod.name })) : [],
+        },
+        {
+
+            labelText: 'Customer',
+            labelId: 'customer',
+            type: 'select',
+            value: customer,
+            required: false,
+            options: Array.isArray(customers) ? customers.map(cust => ({ value: cust.id.toString(), label: cust.name })) : [],
+        },
+    ];
+
+    return (
+        <Form
+            config={config}
+            isLoading={isLoading}
+            btnText='Add Sales Order'
+            onChange={onChange}
+            onSubmit={onSubmit}
+        />
+    );
+}
