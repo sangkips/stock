547ba5a91cba88fcbab721dcfcbb71b0b1ae9b85
sangkips <sangkipkoech@gmail.com>
2024-06-24 18:44:50 +0300

    feat: add sales ui page

---
+++ frontend/app/dashboard/sales/add/page.tsx
@@ -0,0 +1,18 @@
+import { SalesForm } from "@/components/forms";
+
+
+export default function Page() {
+
+
+
+	return (
+		<div className="container mx-auto p-4">
+            <div className="flex justify-between items-center mb-4">
+                <h2 className="text-xl font-semibold">Sales Order</h2>
+            </div>
+            <div className="overflow-x-auto">
+                <SalesForm />
+            </div>
+        </div>
+  );
+}
