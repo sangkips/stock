7dbc8682faeb88579632e6d22cc08f7b66201e55
sangkips <sangkipkoech@gmail.com>
2024-07-04 00:45:24 +0300

    feat: add vercel deployment configs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("PRIMARY_DB"),
+        "USER": getenv("PRIMARY_DB_USER"),
+        "PASSWORD": getenv("PRIMARY_DB_USER_PASSWORD"),
+        "HOST": getenv("PRIMARY_DB_HOST"),
+        "PORT": getenv("PRIMARY_DB_PORT"),
     }
 }
 
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,3 +2,16 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// frontend/next.config.js
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:8000/api/:path*' // Proxy to Django backend
+        }
+      ]
+    }
+  };
+  
